307386e92f9c828aca33e73388502ae7ade5648e
Jialin Liu <mrliujalo@foxmail.com>
2024-10-11 10:08:42 +0800

    set vue host

--- my-vue-app/vite.config.js
+++ my-vue-app/vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',  // 允许所有主机访问
+    port: 3000,       // 根据需要修改端口
+    open: true,       // 自动打开浏览器
+  },
 })
